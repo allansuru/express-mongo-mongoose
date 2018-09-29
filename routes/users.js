164e2a2fcf73b66025b020b8bab1f21b5370c4a4
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2018-09-29 17:43:00 -0300

    middleware

--- routes/users.js
+++ routes/users.js
@@ -11,6 +11,11 @@ router.get('/me', auth, async (req, res) => {
   res.send(user);
 });
 
+router.get('/', async (req, res) => {
+  const users = await User.find().sort('name').select('-password');
+  res.send(users);
+});
+
 router.post('/', async (req, res) => {
   const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
@@ -23,7 +28,6 @@ router.post('/', async (req, res) => {
   user.password = await bcrypt.hash(user.password, salt);
   
   await user.save();
-
   const token = user.generateAuthToken();
   res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email']));
 
